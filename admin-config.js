ab493b5df5081141873b0ea608b637f6cb47fee1
al3-netizen <thevisafox3@gmail.com>
2026-09-03 15:59:53 +0530

    Use verified Gmail as primary admin login

--- admin-config.js
+++ admin-config.js
@@ -1,4 +1,4 @@
 /** Public display configuration only. Never put passwords or secrets here. */
 window.EVISA_ADMIN_CONFIG = {
-  adminEmail: 'alvisa@admin.com'
+  adminEmail: 'thevisafox3@gmail.com'
 };
